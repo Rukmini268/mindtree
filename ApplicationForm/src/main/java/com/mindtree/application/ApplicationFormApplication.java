3f48782e78e343732fe2d62c5648e30faa75c03c
Rukmini268 <46328661+Rukmini268@users.noreply.github.com>
2020-01-13 12:03:48 +0530

    mindtree

---
+++ ApplicationForm/src/main/java/com/mindtree/application/ApplicationFormApplication.java
@@ -0,0 +1,13 @@
+package com.mindtree.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationFormApplication.class, args);
+	}
+
+}
